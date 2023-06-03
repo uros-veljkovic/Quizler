e52da6244185ec3ec73a0d06cbaba47bc04438c7
urkeev14 <uros.veljkovic1996@gmail.com>
2023-06-03 10:08:54 +0200

    In-app update (WIP - not tested) \

--- app/src/main/java/com/example/quizler/feature/main/MainActivity.kt
+++ app/src/main/java/com/example/quizler/feature/main/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.quizler.feature.main
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.quizler.theme.QuizlerTheme
 import com.example.quizler.ui.screen.App
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType.IMMEDIATE
+import com.google.android.play.core.install.model.UpdateAvailability
 
 class MainActivity : ComponentActivity() {
 
     // TODO: Think about how to handle injection without having to
     //  mark all classes with @Inject annotation
-//    @QSendDataToServerWorker
-//    @Inject
-//    lateinit var sendDataToServer: OneTimeWorkRequest
+    //    @QSendDataToServerWorker
+    //    @Inject
+    //    lateinit var sendDataToServer: OneTimeWorkRequest
+
+    private lateinit var appUpdateManager: AppUpdateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +30,49 @@ class MainActivity : ComponentActivity() {
                 App()
             }
         }
+        appUpdateManager = AppUpdateManagerFactory.create(applicationContext)
+        requestUpdateIfExists()
 //        WorkManager.getInstance(this).enqueue(sendDataToServer)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                // If an in-app update is already running, resume the update.
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    IMMEDIATE,
+                    this,
+                    MY_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    private fun requestUpdateIfExists() {
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    IMMEDIATE,
+                    this,
+                    MY_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == MY_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                requestUpdateIfExists()
+            }
+        }
+    }
+
+    companion object {
+        const val MY_REQUEST_CODE = 1234
     }
 }
